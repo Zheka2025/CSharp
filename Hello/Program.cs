74f3c0fa03a5c72810fd91539413c6446857123e
BuildTools <unconfigured@null.spigotmc.org>
2019-11-27 17:29:38 +0300

    game_one

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -11,32 +11,66 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-			Console.ForegroundColor = System.ConsoleColor.Green;
-			Console.Title = "Hello C#";
-			Console.WindowHeight = 10;
-			Console.WindowWidth = 50;
-			Console.BufferWidth = 50;
-			Console.BufferHeight = 10;
-			Console.CursorSize = 100;
-			#region Strings
-			Console.Write("Привет мир\n");
-			Console.WriteLine("сам привет");
-			string name;
-			string age;
-			Console.Write("Введите ваше имя: ");
-			name = Console.ReadLine();
-			Console.Write("сколько вам лет? ");
-			age = Console.ReadLine();
-			//контанация строк:
-			Console.WriteLine("Привет " + name + ", тебе " + age + " лет.");
-			//форматирование строк:
-			Console.WriteLine("Привет {0}, тебе {1} лет", name, age);
-			//интерполяция строк:
-			Console.WriteLine($"Привет {name}, тебе {age} let");
-			Console.WriteLine(@"C:\Windows\System32\");
-			#endregion
-			//System.Console.Clear();
-			
-		}
+            #region
+            Console.ForegroundColor = System.ConsoleColor.Green;
+            //Console.Title = "Hello C#";
+            //Console.WindowHeight = 10;
+            //Console.WindowWidth = 50;
+            //Console.BufferWidth = 50;
+            //Console.BufferHeight = 10;
+            //Console.CursorSize = 100;
+            //#region Strings
+            //Console.Write("Привет мир\n");
+            //Console.WriteLine("сам привет");
+            //string name;
+            //string age;
+            //Console.Write("Введите ваше имя: ");
+            //name = Console.ReadLine();
+            //Console.Write("сколько вам лет? ");
+            //age = Console.ReadLine();
+            ////контанация строк:
+            //Console.WriteLine("Привет " + name + ", тебе " + age + " лет.");
+            ////форматирование строк:
+            //Console.WriteLine("Привет {0}, тебе {1} лет", name, age);
+            ////интерполяция строк:
+            //Console.WriteLine($"Привет {name}, тебе {age} let");
+            //Console.WriteLine(@"C:\Windows\System32\");
+            #endregion
+            //System.Console.Clear();
+            Console.Title = "Game - погоняй квадратик";
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 70;
+            Console.BufferWidth = 70;
+            Console.BufferHeight = 30;
+            string name;
+            Console.Write("Введите ваш ник: ");
+            name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("\t\t\t\t\t\t\t" + name);
+            Console.CursorSize = 100;
+            while (true)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop - 1);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.SetCursorPosition(Console.CursorLeft + 1, Console.CursorTop);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.SetCursorPosition(Console.CursorLeft, Console.CursorTop + 1);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                        break;
+                }
+            }
+
+
+
+
+        }
     }
 }
+
