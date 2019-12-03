ae89bfb4ae8ec67f163315f4e385eb61c81944fb
Student <Student@201-2>
2019-12-03 20:26:22 +0200

    calculator

---
+++ DataTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите выражение: ");
+            string expression = Console.ReadLine();
+            Console.WriteLine(expression);
+            double a = Convert.ToDouble(expression.Split("+-*/".ToCharArray())[0]);
+            double b = Convert.ToDouble(expression.Split("+-*/".ToCharArray())[1]);
+            if(expression.Contains('+'))
+            {
+                Console.WriteLine(expression + " = " + (a + b));
+                Console.WriteLine($"{expression} = {a + b}");
+            }else(expression.Contains('+'))
+            {
+                Console.WriteLine(expression + " = " + (a + b));
+                Console.WriteLine($"{expression} = {a + b}");
+            }if(expression.Contains('+'))
+            {
+                Console.WriteLine(expression + " = " + (a + b));
+                Console.WriteLine($"{expression} = {a + b}");
+            }if(expression.Contains('+'))
+            {
+                Console.WriteLine(expression + " = " + (a + b));
+                Console.WriteLine($"{expression} = {a + b}");
+            }
+            
+                    
+        }
+    }
+}
---
+++ types/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(true.ToString());
+            //Console.WriteLine(Convert.ToInt32(char.MinValue) + "\t" + Convert.ToInt32(char.MaxValue));
+
+            //Console.WriteLine(sizeof(byte));
+
+            //Console.WriteLine(sizeof(float));
+            //Console.WriteLine(float.MinValue + "\t" + float.MaxValue);
+            //Console.WriteLine(sizeof(double));
+            //Console.WriteLine(double.MinValue + "\t" + double.MaxValue);
+            //Console.WriteLine(sizeof(decimal));
+            //Console.WriteLine(decimal.MinValue + "\t" + decimal.MaxValue);
+            //Console.WriteLine ("-----------------------");
+            //byte b = Convert.ToByte(1230);
+            //Console.WriteLine(123.GetType());
+        }
+    }
+}
